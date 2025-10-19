f535da688abf41fdf078525e08150a741fae8f4e
swapnil4885 <swapnilkumavat73@gmail.com>
2025-10-19 11:41:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,4 @@
+TOKEN = "8306875717:AAG34WyLvyi9qvCzQ4mppqUpu3TweHSTrO4"
+SHEET_NAME = "Bot Data"
+SPREADSHEET_URL = "https://docs.google.com/spreadsheets/d/17sooIHix_zMWDLi4YlUGOljXhMcE8Q78h9RnaGfXE7I/edit?usp=drivesdk"
+SERVICE_ACCOUNT_FILE = "service_account.json"
